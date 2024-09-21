ba27abb45516d9513dec30c31f62a6b41540aba3
Mariia_Ostapets <mariyaostapets@gmail.com>
2024-09-21 14:35:18 +0300

    Perform homework 02

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import Description from './Description/Description';
+import Options from './Options/Options';
+import Feedback from './Feedback/Feedback';
+import Notification from './Notification/Notification';
+
+export default function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  return (
+    <div>
+      <Description />
+      <Options
+        feedback={feedback}
+        setFeedback={setFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} />
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
+    </div>
+  );
+}
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,27 @@
+import css from './Options.module.css';
+
+export default function Options({ setFeedback, totalFeedback }) {
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  return (
+    <div className={css.optionsContainer}>
+      <button onClick={() => updateFeedback('good')}>Good</button>
+      <button onClick={() => updateFeedback('neutral')}>Neutral</button>
+      <button onClick={() => updateFeedback('bad')}>Bad</button>
+      {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+    </div>
+  );
+}
